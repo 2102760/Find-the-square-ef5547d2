662d95267df469347ec4b406c47f205437525be2
BeauvR <beauvrmc140@gmail.com>
2020-04-14 12:12:54 +0200

    blast proof

--- square.php
+++ square.php
@@ -1,20 +1,21 @@
 <?php
+
 $input = file_get_contents('input.txt');
 $inputLines = explode("\n", $input);
 
 for ($i = 0; $i < count($inputLines); $i++) {
     $characters = getCharactersFromLine($i);
 
-    for ($j = 0; $j < count($characters); $j++){
+    for ($j = 0; $j < count($characters); $j++) {
         $character = $characters[$j];
-        if ($j + 1 < count($characters) && $i + 1 < count($inputLines)){
+        if ($j + 1 < count($characters) && $i + 1 < count($inputLines)) {
             $nextLineCharacters = getCharactersFromLine($i + 1);
 
             $nextCharacter = $characters[$j + 1];
             $nextLineCharter = $nextLineCharacters[$j];
             $nextLineNextCharter = $nextLineCharacters[$j + 1];
 
-            if ($character === $nextCharacter && $character === $nextLineCharter && $character === $nextLineNextCharter){
+            if ($character === $nextCharacter && $character === $nextLineCharter && $character === $nextLineNextCharter) {
                 $lineReadable = $i + 1;
                 $characterReadable = $j + 1;
                 echo 'found square at line ' . $lineReadable . ' and character ' . $characterReadable;
@@ -24,7 +25,8 @@ for ($i = 0; $i < count($inputLines); $i++) {
     }
 }
 
-function getCharactersFromLine($lineNumber) {
+function getCharactersFromLine($lineNumber)
+{
     $input = file_get_contents('input.txt');
     $lines = explode("\n", $input);
 
